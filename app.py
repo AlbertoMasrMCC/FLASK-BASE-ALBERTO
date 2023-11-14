3f070827f6ef6a78159e4fc151e2789d11b5670f
Manuel Alberto Sotelo Rivas <115024572+AlbertoMasrMCC@users.noreply.github.com>
2023-11-14 21:44:16 +0000

    Agregando endpoint para traducir

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from traducir import traducir_ingles_espanol
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hola, Mundo!</p>"
+
+@app.route("/traducir/<texto>")
+def traducir(texto):
+    return traducir_ingles_espanol(texto)
+
+@app.route("/about")
+def about():
+    return "<h1>Esta es la pagina de about</h1>"
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port=8000)
